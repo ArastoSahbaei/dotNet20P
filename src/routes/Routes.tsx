bfe1b3699a31dfbf1a00ac6bf387909415e32b96
arasto <Arasto.Sahbaei@gmail.com>
2021-04-23 11:56:06 +0200

    added axios

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { HomeView } from '../view/homeview/HomeView'
 import { SignInView } from '../view/signinview/SignInView'
@@ -20,6 +20,9 @@ export const Routes = (props: { children?: React.ReactChild }) => {
 		setAuthenticatedUser(userFromBrowserMemory)
 	}
 
+	const blockRouteIfAuthenticated = (view: React.FC) => { return authenticatedUser ? HomeView : view }
+	const authenticationRequired = (view: React.FC) => { return !authenticatedUser ? SignInView : view }
+
 	useEffect(() => {
 		isUserAuthenticated()
 	})
@@ -28,13 +31,13 @@ export const Routes = (props: { children?: React.ReactChild }) => {
 		<BrowserRouter>
 			{props.children}
 			<Switch>
-				<Route exact path={RoutingPath.signInView} component={SignInView} />
+				<Route exact path={RoutingPath.signInView} component={blockRouteIfAuthenticated(SignInView)} />
 				<Route exact path={RoutingPath.brandsView} component={BrandsView} />
 				<Route exact path={RoutingPath.accessoriesView} component={AccessoriesView} />
 				<Route exact path={RoutingPath.shopView} component={ShopView} />
 				<Route exact path={RoutingPath.newsView} component={NewsView} />
-				<Route exact path={RoutingPath.profileView} component={ProfileView} />
-				<Route exact path={RoutingPath.settingsView} component={SettingsView} />
+				<Route exact path={RoutingPath.profileView} component={authenticationRequired(ProfileView)} />
+				<Route exact path={RoutingPath.settingsView} component={authenticationRequired(SettingsView)} />
 				<Route component={HomeView} />
 			</Switch>
 		</BrowserRouter>
--- src/view/accessoriesview/AccessoriesView.tsx
+++ src/view/accessoriesview/AccessoriesView.tsx
@@ -1,7 +1,25 @@
+import Axios from 'axios'
+import { useState } from 'react'
+
 export const AccessoriesView = () => {
+	const [serverResponse, setServerResponse] = useState<any>()
+	const [counter, setCounter] = useState<number>(1)
+
+	const fetchData = async () => {
+		const API_URL = `https://pokeapi.co/api/v2/pokemon/${counter}`
+		try {
+			const response = await Axios.get(API_URL)
+			setServerResponse(response)
+			setCounter(counter + 1)
+		} catch (error) {
+			alert('error retrieving desired data from server')
+		}
+	}
+
 	return (
 		<div>
-			<h1>This is the accessoriesview</h1>
+			<h1>{serverResponse?.data?.name}</h1>
+			<button onClick={() => fetchData()}>MAKE API CALL</button>
 		</div>
 	)
 }
